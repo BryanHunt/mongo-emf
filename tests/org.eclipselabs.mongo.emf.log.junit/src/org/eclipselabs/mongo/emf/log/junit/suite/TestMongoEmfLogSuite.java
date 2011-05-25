b65678792f011e9541272bf4829716a8d895665c
Bryan Hunt <bhunt@mac.com>
2011-05-24 22:10:22 -0500

    Fix tycho build

---
+++ tests/org.eclipselabs.mongo.emf.log.junit/src/org/eclipselabs/mongo/emf/log/junit/suite/TestMongoEmfLogSuite.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Bryan Hunt.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bryan Hunt - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipselabs.mongo.emf.log.junit.suite;
+
+import org.eclipselabs.mongo.emf.log.junit.tests.TestLogService;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestLogService.class })
+public class TestMongoEmfLogSuite
+{}
