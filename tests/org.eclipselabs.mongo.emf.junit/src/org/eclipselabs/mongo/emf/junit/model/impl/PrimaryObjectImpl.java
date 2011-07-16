0df37ce2fee17b8868f3e2bb49f20a72aff9ccec
Bryan Hunt <bhunt@mac.com>
2011-07-15 23:26:30 -0500

    Updated to JUnit 4.9

--- tests/org.eclipselabs.mongo.emf.junit/src/org/eclipselabs/mongo/emf/junit/model/impl/PrimaryObjectImpl.java
+++ tests/org.eclipselabs.mongo.emf.junit/src/org/eclipselabs/mongo/emf/junit/model/impl/PrimaryObjectImpl.java
@@ -16,7 +16,6 @@ import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
 import org.eclipse.emf.ecore.util.BasicFeatureMap;
-import org.eclipse.emf.ecore.util.EDataTypeUniqueEList;
 import org.eclipse.emf.ecore.util.EObjectContainmentEList;
 import org.eclipse.emf.ecore.util.EObjectResolvingEList;
 import org.eclipse.emf.ecore.util.FeatureMap;
