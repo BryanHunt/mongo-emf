3df2b6500a6cf14a7b2b500d63080e5523e9feea
Bryan Hunt <bhunt@mac.com>
2012-01-14 20:58:57 -0600

    Updated the copyright

--- bundles/org.eclipselabs.mongo.emf/src/org/eclipselabs/mongo/emf/EObjectBuilder.java
+++ bundles/org.eclipselabs.mongo.emf/src/org/eclipselabs/mongo/emf/EObjectBuilder.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2012 Bryan Hunt.
+ * Copyright (c) 2012 Bryan Hunt & Ed Merks.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Bryan Hunt - initial API and implementation
+ *    Bryan Hunt & Ed Merks - initial API and implementation
  *******************************************************************************/
 
 package org.eclipselabs.mongo.emf;
