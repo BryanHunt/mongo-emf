7d6bfe74611ec0102b1e4b532076a3d18466eb0b
Bryan Hunt <bhunt@mac.com>
2012-04-18 20:57:32 -0500

    Added documentation.

--- bundles/org.eclipselabs.mongo.emf.developer.junit/src/org/eclipselabs/mongo/emf/developer/junit/ServiceTestHarness.java
+++ bundles/org.eclipselabs.mongo.emf.developer.junit/src/org/eclipselabs/mongo/emf/developer/junit/ServiceTestHarness.java
@@ -16,11 +16,32 @@ import static org.junit.Assert.assertTrue;
 import org.junit.BeforeClass;
 
 /**
+ * This class is intended to be extended by clients creating unit tests in an OSGi environment
+ * that requires services to be activated before the unit test runs. Clients should register
+ * the extension of this class as a declarative service depending on the services required
+ * services. If the JUnit test uses one of the services, the service bind function should
+ * assign the service to a static data member.
+ * 
+ * The JUnit framework will automatically call waitForServices() on this class. This function
+ * will wait until the OSGi declarative services manager calls activate() before allowing the
+ * unit test to continue. The waitForServices() call has a built-in timout of 60 seconds.
+ * This timeout will keep your automated builds from hanging in the case that a service is not
+ * bound.
+ * 
+ * This pattern was taken from the book OSGi and Equinox: Creating Highly Modular Java Systems,
+ * Jeff McAffer, Paul VanderLei, and Simon Archer, Pearson Education, Inc, page 135.
+ * 
  * @author bhunt
  * 
  */
 public class ServiceTestHarness
 {
+	/**
+	 * Called automatically by the JUnit framework once for all unit tests in the extending class.
+	 * Clients should not have to call this function directly.
+	 * 
+	 * @throws InterruptedException if any thread interrupts the thread waiting for the activation
+	 */
 	@BeforeClass
 	public static void waitForServices() throws InterruptedException
 	{
@@ -33,6 +54,10 @@ public class ServiceTestHarness
 		}
 	}
 
+	/**
+	 * Called automatically by the OSGi declarative services manager. Clients should not have to call this
+	 * function directly.
+	 */
 	protected void activate()
 	{
 		synchronized (lock)
