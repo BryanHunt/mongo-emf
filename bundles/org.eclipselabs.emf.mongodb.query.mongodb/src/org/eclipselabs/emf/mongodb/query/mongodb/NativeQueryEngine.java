4a33d596f79370af24a84597af6d565fe0ef4961
Bryan Hunt <bhunt@mac.com>
2013-07-19 22:12:13 -0500

    Really added native query bundle

---
+++ bundles/org.eclipselabs.emf.mongodb.query.mongodb/src/org/eclipselabs/emf/mongodb/query/mongodb/NativeQueryEngine.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Bryan Hunt.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bryan Hunt - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipselabs.emf.mongodb.query.mongodb;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipselabs.emf.mongodb.QueryEngine;
+import org.eclipselabs.emf.mongodb.model.ModelFactory;
+import org.eclipselabs.emf.mongodb.model.MongoQuery;
+
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+
+/**
+ * @author bhunt
+ * 
+ */
+public class NativeQueryEngine implements QueryEngine
+{
+	@Override
+	public MongoQuery buildDBObjectQuery(URI uri)
+	{
+		DBObject query = (DBObject) JSON.parse(URI.decode(uri.query()));
+
+		MongoQuery mongoQuery = ModelFactory.eINSTANCE.createMongoQuery();
+		mongoQuery.setFilter((DBObject) query.get("filter"));
+		mongoQuery.setProjection((DBObject) query.get("projection"));
+		mongoQuery.setSort((DBObject) query.get("sort"));
+		mongoQuery.setLimit((Integer) query.get("limit"));
+
+		return mongoQuery;
+	}
+}
